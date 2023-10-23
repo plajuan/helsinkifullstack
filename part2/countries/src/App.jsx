9f873e0586f988272a175fb4302ade06de396a6a
plajuan <pla.juan@gmail.com>
2023-10-22 21:53:26 -0300

    First commit

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+    
+  return (
+    <div>
+      find countries <input type="text" name="country" id="country" />
+    </div>  
+    //TODO Impl https://studies.cs.helsinki.fi/restcountries/ in axios
+    //exercise 2.18 
+    //https://fullstackopen.com/en/part2/adding_styles_to_react_app
+  )
+}
+
+export default App
