830d8374b21ca240fcd6dd49601fa486a8219070
plajuan <pla.juan@gmail.com>
2023-10-24 22:08:10 -0300

    FIXME: CapitalWeather

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -12,6 +12,7 @@ const Filter = (props) =>{
 }
 
 const CoutriesList = (props) => {
+  //props.setCapital('')
   if (props.filterText[0] === ''){
     return
   }
@@ -28,51 +29,71 @@ const CoutriesList = (props) => {
     return (
       <>      
         {countries.map(x => { return (          
-          <p key={x.name.common+"p"} id={x.name.common+"p"}>
+          <p key={x.name.common} id={x.name.common}>
             {x.name.common}
             &nbsp; 
-            <button type="button" onClick={props.showCountry} key={x.name.common} id={x.name.common}>show</button>
+            <button type="button" onClick={props.showCountry} id={x.name.common}>show</button>
           </p>
         )        
         })}      
       </>
     )
   }
-  if (countries.length == 1){    
+  if (countries.length == 1){
+    const country = countries[0]
+    props.setCapital(country.capital[0])    
     return (
       <>
-      { countries.map(x => {        
-        return (
-          <>
-          <h3>{x.name.common}</h3>
-          <p>
-            capital&nbsp;{x.capital.map(x => x).join(' ')} <br></br>
-            area&nbsp;{x.area}            
+      <h3>{country.name.common}</h3>
+      <p key='p1'>
+            capital&nbsp;{country.capital.map(x => x).join(' ')} <br></br>
+            area&nbsp;{country.area}
           </p>
           <h4>languages</h4>
           <ul>
-            {Object.keys(x.languages).map( (key, index) => {
+            {Object.keys(country.languages).map( (key, index) => {
               return (
                 <li key={index}>                  
-                    {x.languages[key]}      
+                    {country.languages[key]}      
                 </li>
               );
             }
             )}
           </ul>          
-          <img src={x.flags.svg} alt={x.name.common} width='20%' />
-          </>
-        )
-      }) }
-      </>
-      
+          <img src={country.flags.svg} alt={country.name.common} width='20%' />
+      </>      
     )
+  } else {
+    props.setCapital('')
+    return
   }
   
 }
 
+const CapitalWeather = (props) => {
+  console.log('capital', props.capital)
+  if (!props.capital || props.capital.length === 0){
+    return
+  }
+  
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${props.capital}&appid=${import.meta.env.VITE_WEATHER_API}`
+  axios.get(url)
+    .then(x=> {      
+      return (
+        <>
+        <h3>Weather in {props.capital}</h3>
+        <p>temperature {x.data.main.temp} fahreheinght</p>
+        <img src={`https://openweathermap.org/img/wn/${x.data.weather[0].icon}@2x.png`}/>
+        <p>wind {x.data.wind.speed} m/s</p>
+        </>
+      )
+    })
+    .catch(x=>console.error('ERROR', x))
+  
+}
+
 const App = () => {
-  const [countries, setCountries] = useState([])  
+  const [countries, setCountries] = useState([])
   const getCountries = ()=>{
     axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
     .then(x => {
@@ -80,24 +101,25 @@ const App = () => {
     } )
     .catch( () => {alert('Error')})
   }
-  const config = []
-  useEffect(getCountries, config)
+  
+  useEffect(getCountries, [])
 
   const [filterText, setFilterText] = useState(['', '0'])
   const filterTextChange = (event) => {
     setFilterText([event.target.value,'0'])
   }
-
  
-  const showCountry = (event)=>{
-    console.log(event.target.id)
+  const showCountry = (event)=>{    
     setFilterText([event.target.id,'1'])
   }
+
+  const [capital, setCapital] = useState('')
 
   return (
     <div>
       <Filter filterTextChange={filterTextChange} filterText={filterText} />
-      <CoutriesList countries={countries} filterText={filterText} showCountry={showCountry}/>
+      <CoutriesList countries={countries} filterText={filterText} showCountry={showCountry} setCapital={setCapital} />
+      <CapitalWeather capital={capital} />
     </div>  
     
     //https://fullstackopen.com/en/part2/adding_styles_to_react_app
@@ -105,3 +127,48 @@ const App = () => {
 }
 
 export default App
+
+/*
+const medelete = {
+    "coord": {
+        "lon": -0.13,
+        "lat": 51.51
+    },
+    "weather": [
+        {
+            "id": 300,
+            "main": "Drizzle",
+            "description": "light intensity drizzle",
+            "icon": "09d"
+        }
+    ],
+    "base": "stations",
+    "main": {
+        "temp": 280.32,
+        "pressure": 1012,
+        "humidity": 81,
+        "temp_min": 279.15,
+        "temp_max": 281.15
+    },
+    "visibility": 10000,
+    "wind": {
+        "speed": 4.1,
+        "deg": 80
+    },
+    "clouds": {
+        "all": 90
+    },
+    "dt": 1485789600,
+    "sys": {
+        "type": 1,
+        "id": 5091,
+        "message": 0.0103,
+        "country": "GB",
+        "sunrise": 1485762037,
+        "sunset": 1485794875
+    },
+    "id": 2643743,
+    "name": "London",
+    "cod": 200
+}
+*/
